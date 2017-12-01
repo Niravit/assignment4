b57f597d83c41a0a5b605e5a5863c670d9b46ec0
Niravit <1611030005@students.stamford.edu>
2017-12-01 12:14:46 +0700

    assignment4

---
+++ Niravit.js
@@ -0,0 +1,18 @@
+var customername = "Niravit"
+var price = 10;
+var quantity = 3;
+var totalPrice = price * quanttity;
+document.getElementById("customer-name").innerHTML = "Niravit";
+document.getElementById("price").innerHTML = "$"+ totalPrice;
+var products = [
+ "Brownie",
+ "Blackout cake", 	
+ "Red velvet cake",
+ "Depression cake",
+ "Caterpillar cake" 
+
+];
+var price = [15,20,30,80,100];
+
+var productsText = "";
+var productsText
